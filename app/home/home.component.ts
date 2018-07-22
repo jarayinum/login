44b78553181091306fafad2918d3876f684b3b03
jarayinum <40541644+jarayinum@users.noreply.github.com>
2018-07-22 17:43:09 +0530

    Add files via upload

---
+++ app/home/home.component.ts
@@ -0,0 +1,36 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { User } from '../_models/index';
+import { UserService } from '../_services/index';
+
+@Component({
+    moduleId: module.id,
+
+    templateUrl: 'home.component.html',
+    
+})
+
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    users: User[] = [];
+
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+    today: number = Date.now();
+    ngOnInit() {
+        this.loadAllUsers();
+        
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id).subscribe(() => { this.loadAllUsers() });
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+
+
+}
+
